6c922ed5282b47c0f612db73a83b87e72852492a
Lee.Hyunsup <clocktree@live.co.kr>
2019-12-18 20:22:12 +0900

    20191218_9
    - 수정.

--- js/move.js
+++ js/move.js
@@ -10,9 +10,9 @@ $(function(){
 				slideNum++;
 				$('.slide-container').append('<li class="slide" id="slide'+slideNum+'"><img src='+item.img_url+' alt=""></li>');
 				$('.indicator').append('<li id="bulet'+slideNum+'" class="bulet">●</li>');
-				msheight = $('.slide img').height();
+				msheight = $('.slide').height();
 				$('.slide-wrap').css({'height':msheight});
-				console.log(msheight);
+				console.log('li - '+msheight);
 				mswidth = $('.slide').each(Array).length;/*슬라이드 전체 배열의 갯수만큼의 숫자를 추출*/
 				for (var i=0;i<mswidth;i++)/*.slide의 배열이 늘어나면 알아서 아이디와 레프트값 연산 및 .indicator의 btn도 배열 갯수만큼 append*/
 				{
